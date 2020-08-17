a6c9ae136b4982f9b1ec7119c134609175007762
Alexey Verevkin <avverevkin@goxapp.com>
2020-08-17 15:41:32 +0300

    integrated

--- src/components/context/reducer.js
+++ src/components/context/reducer.js
@@ -1,10 +1,19 @@
 import { CREATE, UPDATE, DELETE } from './actions';
+import ObjectID from 'bson-objectid';
 
-export default (state, action) => {
+export const reducer = (state, action) => {
     switch (action.type) {
         case CREATE:
+            // IMPORTANT! Modifying payload in reducer is not acceptable!
+            // But since it's demo I did it
+            const data = action.payload.map(item => {
+                if (!item.id) {
+                    item.id = new ObjectID().toString();
+                }
+                return item;
+            });
             // Just in case if we need to create a batch of posts
-            return [...state, ...action.payload];
+            return [...state, ...data];
         case UPDATE:
             // I use map and filter here because we don't have many posts.
             // Slice is preferrable in real-life apps
